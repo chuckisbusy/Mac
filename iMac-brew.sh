6350e55987d20dde03950b076d857b3b1eb6e673
chuckisbusy <chuckisbusy@icloud.com>
2019-06-12 18:26:10 -0400

    Update iMac-brew.sh

--- iMac-brew.sh
+++ iMac-brew.sh
@@ -8,11 +8,11 @@ else
     echo "homebrew is installed"
 fi
 
-brew install python python3 p7zip wget unrar duti bash tree rename screen zsh
+brew install python python3 p7zip wget unrar duti bash tree rename screen zsh osxfuse
 brew tap buo/cask-upgrade
 brew tap caskroom/cask
-brew install Caskroom/cask/xquartz
-brew install grace mas smartmontools
+brew install xquartz
+brew install grace mas smartmontools sshfs
 
 mas install 883878097 #Server(5.3.1)
 mas install 1278508951 #Trello(2.9.7)
