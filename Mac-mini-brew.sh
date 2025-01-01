38a1edcc5e6b8939e69943f4c9048a27c8bf8113
chuckisbusy <chuckisbusy@icloud.com>
2025-01-01 11:59:22 -0500

    Update brew.sh for Seqouia

--- Mac-mini-brew.sh
+++ Mac-mini-brew.sh
@@ -1,55 +1,104 @@
 #!/bin/bash
 #
 xcode-select --install
-if [ ! -x /usr/local/bin/brew ]; then
-    echo "installing homebrew"
-    /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    echo "homebrew is installed"
-fi
-
-brew install python python3 p7zip wget unrar duti bash tree rename screen zsh
-brew tap buo/cask-upgrade
-brew tap caskroom/cask
 brew install mas
+brew install cask wget duti bash tmux rename screen zsh grace python@3.11 mist-cli
 
-mas install 409907375 #Remote Desktop (3.9)
-#mas install 497799835 Xcode (9.1)
-mas install 425424353 #The Unarchiver (3.11.3)
-mas install 409183694 #Keynote (7.3.1)
-mas install 408981434 #iMovie (10.1.8)
-mas install 967004861 #HP Easy Scan (1.9.1)
-mas install 510232205 #The Archive Browser (1.11.1)
-mas install 443987910 #1Password (6.8.4)
-mas install 979299240 #NetworkUtilityX (6.0.2)
-mas install 883878097 #Server (5.3.1)
-mas install 403504866 #PCalc (4.5.2)
-mas install 1224268771 #Screens 4 (4.3.1)
-mas install 1026349850 #Copied (2.0.6)
-mas install 409201541 #Pages (6.3.1)
-mas install 409203825 #Numbers (4.3.1)
-mas install 1148868081 #iStat View (3.03)
+mas install 1033342465  # Just Press Record                           (50.0)
+mas install 430255202   # Mactracker                                  (7.13.2)
+mas install 979299240   # Network Kit                                 (9.2.0)
+mas install 1153157709  # Speedtest                                   (1.27)
+mas install 403304796   # iNet Network Scanner                        (3.1.5)
+mas install 6443941139  # 2FAS - Two Factor Authentication Extension  (1.7.3)
+mas install 1529448980  # Reeder                                      (5.5)
+mas install 1290358394  # Cardhop                                     (2.3.3)
+mas install 975937182   # Fantastical                                 (4.0.3)
+mas install 858446756   # World Clock Pro                             (1.7.11)
+mas install 1569813296  # 1Password for Safari                        (8.10.56)
+mas install 403504866   # PCalc                                       (4.10.8)
+mas install 1233048948  # Boom 3D                                     (2.2.3)
+mas install 1224268771  # Screens                                     (4.12.16)
+mas install 969714962   # TV Time                                     (2.3.2)
+mas install 1542143627  # OmniFocus                                   (4.5.1)
+mas install 1055273043  # PDF Expert                                  (3.10.10)
+mas install 1289583905  # Pixelmator Pro                              (3.6.14)
+mas install 1136220934  # Infuse                                      (8.0.8)
+mas install 1348635544  # Moon Calendar for menu bar                  (1.8.1)
+mas install 1438243180  # Dark Reader for Safari                      (2.1.1)
+mas install 1471849104  # Periodic Table Chemistry 2025M              (4.6)
+mas install 441258766   # Magnet                                      (3.0.6)
+mas install 494803304   # WiFi Explorer                               (3.5.3)
+mas install 823528286   # Folx GO+                                    (5.27)
+mas install 1663047912  # Screens 5                                   (5.4.9)
+mas install 1195676848  # Grocery                                     (3.1.4)
+mas install 409201541   # Pages                                       (14.3)
+mas install 979561272   # Just Press Record                           (19.3)
+mas install 1492055171  # Expenses                                    (3.6.23)
+mas install 803453959   # Slack                                       (4.41.105)
+mas install 747648890   # Telegram                                    (11.5)
+mas install 1594420480  # Prompt                                      (3.0.8)
+mas install 457622435   # Yoink                                       (3.6.98)
+mas install 639968404   # Parcel                                      (7.14.2)
+mas install 1480068668  # Messenger                                   (226.0)
+mas install 1463334954  # SessionRestore                              (2.8.2)
+mas install 409203825   # Numbers                                     (14.3)
+mas install 1193539993  # Brother iPrint&Scan                         (12.1.0)
+mas install 409183694   # Keynote                                     (14.3)
 
-brew cask install adobe-acrobat-reader #acrobat reader
-brew cask install transmit          #Transmit
-brew cask install google-chrome 	#Google Chrome
-brew cask install istat-server 		#iStat Server
-brew cask install screens-connect 	#Screen Connect
-brew cask install sublime-text 		#Sublime Text
-brew cask install transmission 		#Transmission
+brew install --cask 1password
+brew install --cask adguard
+brew install --cask alfred
+brew install --cask appcleaner
+brew install --cask betterzip
+brew install --cask calibre
+brew install --cask cookie
+brew install --cask discord
+brew install --cask dropbox
+brew install --cask fantastical
+brew install --cask github
+brew install --cask google-chrome
+brew install --cask jordanbaird-ice
+brew install --cask popclip
+brew install --cask mendeley
+brew install --cask obsidian
+brew install --cask screens-connect
+brew install --cask skype
+brew install --cask soulver 
+brew install --cask spotify
+brew install --cask sublime-text
+brew install --cask transmission
+brew install --cask transmit
+brew install --cask vlc
+brew install --cask xld
+brew install --cask xquartz
+brew install --cask zotero
+brew install --cask zoom
 brew cleanup
 
+defaults delete com.apple.dock; killall Dock
 defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
-echo 'Add this to /etc/shells:
 
-sudo nano /etc/shells
-/usr/local/bin/bash
-/usr/local/bin/zsh'
-sleep 60s
-sudo vi /etc/shells
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sudo easy_install Pygments
-echo 'Add plugins=() to .zshrc'
+echo 'Install from Search:
+Logitech (Options(MPB),Gaming(iMac),Spotlight Presentation)
+OSXFuse
+SSHFS
+Microsoft Office
+Adobe Creative Cloud
+PyMol
+Latex
+Dash
+Mediainfo
+Spek
+Yates
+VMD'
+
+sudo pip3 install Pygments
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/vimrc.txt -O ~/.vimrc
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/tmux.conf.txt -O ~/.tmux.conf
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/bash_profile.txt -O ~/.bash_profile
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/bashrc.txt -O ~/.bashrc
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/pymolrc.txt -O ~/.pymolrc
+wget -c https://raw.githubusercontent.com/chuckisbusy/dotfiles/refs/heads/master/Mac/screenrc.txt -O ~/.screenrc
+
+echo DONE
+echo 'Add to .zshrc'
